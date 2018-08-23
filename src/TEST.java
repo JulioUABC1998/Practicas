de8aab36b7b938bf0908118473fa55173502531e
JulioUABC1998 <42424222+JulioUABC1998@users.noreply.github.com>
2018-08-22 18:38:30 -0700

    Update TEST.java

--- src/TEST.java
+++ src/TEST.java
@@ -5,9 +5,12 @@ public class TEST {
 	System.out.println(Operaciones.MULTIPLICACION);	
 	System.out.println(Operaciones.SUMA.getValor());
 	System.out.println(Operaciones.SUMA.name());
-		for(Operaciones elemento:Operaciones.values())
-		{
-		System.out.println(elemento.getValor());
+	Operaciones[] values = Operaciones.values();
+		for (int i = 0; i < values.length; i++) {
+			Operaciones elemento = values[i];
+		}Operaciones[] values = Operaciones.values();
+		for (int i = 0; i < values.length; i++) {
+			Operaciones elemento = values[i];
 		}
 	}
 }
