f2fdcdb09a20169be90c0a8e7490078df830deaf
Alumno <Alumno@ws204-03>
2018-08-22 18:55:44 -0700

    commit and push

--- src/TEST.java
+++ src/TEST.java
@@ -5,21 +5,12 @@ public class TEST {
 	System.out.println(Operaciones.MULTIPLICACION);	
 	System.out.println(Operaciones.SUMA.getValor());
 	System.out.println(Operaciones.SUMA.name());
-//<<<<<<< HEAD
+
 		for(Operaciones elemento:Operaciones.values())
 		{
 		System.out.println(elemento.getValor());
 		System.out.println(elemento.name());
 		
-//=======
-	Operaciones[] values = Operaciones.values();
-		for (int i = 0; i < values.length; i++) {
-			Operaciones elemento = values[i];
-		}Operaciones[] values = Operaciones.values();
-		for (int i = 0; i < values.length; i++) {
-			Operaciones elemento = values[i];
-//>>>>>>> branch 'master' of https://github.com/JulioUABC1998/Practicas.git
-		}
 	}
 	}
 }
