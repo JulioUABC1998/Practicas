88995aa341e99fb6aef8d3bef30dac020fa5a253
Alumno <Alumno@ws204-03>
2018-08-22 18:23:40 -0700

    actualizacion

--- src/TEST.java
+++ src/TEST.java
@@ -3,6 +3,7 @@ public class TEST {
 	public static void main(String[] args)
 	{
 	System.out.println(Operaciones.MULTIPLICACION);	
-		
+	System.out.println(Operaciones.SUMA.getValor());
+	System.out.println(Operaciones.SUMA.name());
 	}
 }
