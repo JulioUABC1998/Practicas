73e857d58429508432f750700715e54bf07504b1
Alumno <Alumno@ws204-03>
2018-08-22 18:36:01 -0700

    for agregado

--- src/TEST.java
+++ src/TEST.java
@@ -5,5 +5,9 @@ public class TEST {
 	System.out.println(Operaciones.MULTIPLICACION);	
 	System.out.println(Operaciones.SUMA.getValor());
 	System.out.println(Operaciones.SUMA.name());
+		for(Operaciones elemento:Operaciones.values())
+		{
+		System.out.println(elemento.getValor());
+		}
 	}
 }
